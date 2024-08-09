cc18e8938b24a4cfb2e49d1c8c40a26aaa995de9
Lee Dickenson <lee.dickenson@latitudefinancial.com>
2024-08-09 13:13:52 +1000

    Basic test of existing behaviour of each endpoint

--- src/test/java/com/audition/AuditionApplicationTests.java
+++ src/test/java/com/audition/AuditionApplicationTests.java
@@ -1,15 +1,38 @@
 package com.audition;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AuditionApplicationTests {
 
-    // TODO implement unit test. Note that an applicant should create additional unit tests as required.
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void shouldGetAllPosts() throws Exception {
+        this.mockMvc.perform(get("/posts"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("[]")));
+    }
+
+    @Test
+    void shouldGetPostWithId() throws Exception {
+        this.mockMvc.perform(get("/post/1234"))
+            .andExpect(status().isNotFound())
+            .andExpect(content().string(containsString("")));
+    }
 }
